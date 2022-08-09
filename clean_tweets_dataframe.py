deb80c5d178b75d31ccfa4966d7ea1b0256977a0
lewiEyasu <eyasulewi@gmail.com>
2022-08-09 20:45:47 +0300

    fixed bug on the function convert_to_datetime

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -36,23 +36,19 @@ class Clean_Tweets:
         
         return df
         
-        
-        
-        df = df[df['created_at'] >= '2020-12-31' ]
-        
-        return df
-    
+
+
     def convert_to_numbers(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        df['polarity'] = pd.----
-        
-        ----
-        ----
+        numbers_columns = ["polarity", "subjectivity", "retweet_count", "favorite_count", "followers_count", "friends_count" ]
+        for col in  numbers_columns :
+            df[col] = pd.to_numeric(df[col])
         
         return df
+        
     
     def remove_non_english_tweets(self, df:pd.DataFrame)->pd.DataFrame:
         """
