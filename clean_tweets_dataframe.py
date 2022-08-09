af74103cc7338686149f4bba291efc427452cdd5
lewiEyasu <eyasulewi@gmail.com>
2022-08-09 20:44:01 +0300

    fixed bug on the function drop_duplicate

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -8,17 +8,17 @@ class Clean_Tweets:
         self.df = df
         print('Automation in Action...!!!')
         
-    def drop_unwanted_column(self)->pd.DataFrame:
+    def drop_unwanted_column(self, df:pd.DataFrame)->pd.DataFrame:
         """
         remove rows that has column names. This error originated from
         the data collection stage.  
         """
         unwanted_rows = df[df['retweet_count'] == 'retweet_count' ].index
-        self.df.drop(unwanted_rows , inplace=True)
-        self.df = self.df[self.df['polarity'] != 'polarity']
+        df.drop(unwanted_rows , inplace=True)
+        df = df[df['polarity'] != 'polarity']
         
         return df
-    def drop_duplicate(self)->pd.DataFrame:
+    def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
         """
         drop duplicate rows
         """
@@ -26,19 +26,23 @@ class Clean_Tweets:
         df.drop_duplicates().reset_index()
         
         return df
-    def convert_to_datetime(self)->pd.DataFrame:
+    def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
         """
-        ----
+        df['created_at']= pd.to_datetime(df['created_at'])
         
-        ----
+        df = df[df['created_at'] >= '2020-12-31' ]
+        
+        return df
+        
+        
         
         df = df[df['created_at'] >= '2020-12-31' ]
         
         return df
     
-    def convert_to_numbers(self)->pd.DataFrame:
+    def convert_to_numbers(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
@@ -50,7 +54,7 @@ class Clean_Tweets:
         
         return df
     
-    def remove_non_english_tweets(self)->pd.DataFrame:
+    def remove_non_english_tweets(self, df:pd.DataFrame)->pd.DataFrame:
         """
         remove non english tweets from lang
         """
