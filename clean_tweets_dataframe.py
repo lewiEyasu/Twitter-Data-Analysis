335b24072ee4ca3da0f407d352579ea47395e479
lewiEyasu <eyasulewi@gmail.com>
2022-08-09 20:49:42 +0300

    fixed bug on the function convert_to_numbers and remove_non_english_tweets

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -54,7 +54,7 @@ class Clean_Tweets:
         """
         remove non english tweets from lang
         """
-        
-        df = ----
+                
+        df = df[(df['lang'] == "en")]
         
         return df
